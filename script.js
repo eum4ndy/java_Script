00df28f78fd990672f65457d2f387654092df1eb
eum4ndy <163136667+eum4ndy@users.noreply.github.com>
2024-10-16 08:26:28 -0300

    MANDY PART2

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function mudarcor(){
-    const titulo = document.getElementById(`titulo`);
-    titulo.style.color = titulo.style.color === `Red` ? `Blue`: `red`
- }
+function alterarCirculo() {
+    const circulo = document.getElementById('circulo');
+    const corAleatoria = '#' +
+    Math.floor(Math.random()*16777215).toString(16);
+    const novoTamanho = Math.floor(Math.random() * (200-50 + 1)) + 50;
+circulo.style.backgroundColor = corAleatoria;
+circulo.style.width = novoTamanho + 'px';
+circulo.style.height = novoTamanho + 'px';
+}
